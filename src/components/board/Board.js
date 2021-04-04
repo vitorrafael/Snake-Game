5b4a4053db669dd6e08122600fead7624b0eeab7
vitorrafael <vrsilveira07@gmail.com>
2021-04-04 17:49:42 -0300

    Adds Snake Growth Handler

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { generateRandomNumberInRange } from '../../utils/Utils';
 import { DIRECTION } from '../../enums/Direction';
 
 import './Board.css'
@@ -29,15 +30,17 @@ export class Board extends React.Component {
         super(props);
 
         const board = createBoard(BOARD_SIZE);
-        const snake = new LinkedList({row: STARTING_ROW, column: STARTING_COLUMN, cell: board[STARTING_ROW][STARTING_COLUMN]});
+        const snake = new LinkedList({ row: STARTING_ROW, column: STARTING_COLUMN, cell: board[STARTING_ROW][STARTING_COLUMN] });
         const snakeCells = new Set([snake.head.value.cell]);
+        const foodCell = snake.head.value.cell + 5;
         const direction = DIRECTION.RIGHT;
 
         this.state = {
             board: board,
             snake: snake,
             snakeCells: snakeCells,
-            direction: direction
+            direction: direction,
+            foodCell: foodCell
         }
     }
 
@@ -62,7 +65,6 @@ export class Board extends React.Component {
             return;
         }
 
-
         const nextHeadCell = this.state.board[nextHeadCoordinates.row][nextHeadCoordinates.column];
         const newHead = new LinkedListNode({
             row: nextHeadCoordinates.row,
@@ -80,7 +82,46 @@ export class Board extends React.Component {
 
         snake.tail = snake.tail.next || snake.head;
 
-        this.setState({snakeCells: newSnakeCells});
+        const foodConsumed = nextHeadCell === this.state.foodCell;
+        if (foodConsumed) {
+            this.handleFoodConsumption(newSnakeCells);
+            this.growSnake(newSnakeCells);
+        }
+
+        this.setState({ snakeCells: newSnakeCells });
+    }
+
+    growSnake(snakeCells) {
+        const { snake, direction, board } = this.state;
+        const growthNodeCoordinates = this.getGrowthNodeCoordinates(snake.tail, direction);
+
+        if (this.isOutOfBounds(growthNodeCoordinates, board)) return;
+
+        const newTailCell = board[growthNodeCoordinates.row][growthNodeCoordinates.column];
+        const newTail = new LinkedListNode({
+            row: growthNodeCoordinates.row,
+            column: growthNodeCoordinates.column,
+            cell: newTailCell
+        });
+
+        const currentTail = snake.tail;
+        snake.tail = newTail;
+        snake.tail.next = currentTail;
+
+        snakeCells.add(newTailCell);
+    }
+
+    handleFoodConsumption(snakeCells) {
+        const maxPossibleCellValue = BOARD_SIZE * BOARD_SIZE;
+
+        let nextFoodCell;
+        while (true) {
+            nextFoodCell = generateRandomNumberInRange(1, maxPossibleCellValue);
+            if (snakeCells.has(nextFoodCell) || this.state.foodCell === nextFoodCell) continue;
+            else break;
+        }
+
+        this.setState({ foodCell: nextFoodCell });
     }
 
     handleKeydown(event) {
@@ -120,16 +161,41 @@ export class Board extends React.Component {
         }
     }
 
+
+    getGrowthNodeCoordinates(snakeTail, currentDirection) {
+        const tailNextNodeDirection = getNextNodeDirection(snakeTail, currentDirection);
+        const growthDirection = getOppositeDirection(tailNextNodeDirection);
+
+        const currentTailNodeCoordinates = {
+            row: snakeTail.value.row,
+            column: snakeTail.value.column
+        };
+
+        const growthNodeCoordinates = this.getCoordinatesInDirection(currentTailNodeCoordinates, growthDirection);
+
+        return growthNodeCoordinates;
+    }
+
     handleGameOver() {
-        const snake = new LinkedList({row: STARTING_ROW, column: STARTING_COLUMN, cell: this.state.board[STARTING_ROW][STARTING_COLUMN]});
+        const snake = new LinkedList({ row: STARTING_ROW, column: STARTING_COLUMN, cell: this.state.board[STARTING_ROW][STARTING_COLUMN] });
         const snakeCells = new Set([snake.head.value.cell]);
         const direction = DIRECTION.RIGHT;
+        const foodCell = snake.head.value.cell + 5;
 
         this.setState({
-            snake:  snake,
-            snakeCells: snakeCells,
-            direction: direction
+            snake,
+            snakeCells,
+            direction,
+            foodCell
         });
+    }
+
+    getCellClassName(cellValue, foodCell, snakeCells) {
+        let className = 'cell';
+        if (cellValue === foodCell) className = 'cell food-cell';
+        if (snakeCells.has(cellValue)) className = 'cell snake-cell';
+
+        return className;
     }
 
     componentDidMount() {
@@ -137,7 +203,7 @@ export class Board extends React.Component {
 
         setInterval(() => {
             this.moveSnake();
-        }, 250);
+        }, 150);
     }
 
     render() {
@@ -147,7 +213,8 @@ export class Board extends React.Component {
                     {this.state.board.map((row, rowIndex) => (
                         <div key={rowIndex} className="row">
                             {row.map((cellValue, cellIndex) => {
-                                const className = this.state.snakeCells.has(cellValue) ? "cell snake-cell" : "cell";
+                                const { foodCell, snakeCells } = this.state;
+                                const className = this.getCellClassName(cellValue, foodCell, snakeCells);
                                 return <div key={cellIndex} className={className}></div>;
                             })}
                         </div>
@@ -156,6 +223,27 @@ export class Board extends React.Component {
             </>
         )
     }
+}
+
+
+function getOppositeDirection(direction) {
+    if (direction === DIRECTION.UP) return DIRECTION.DOWN;
+    if (direction === DIRECTION.RIGHT) return DIRECTION.LEFT;
+    if (direction === DIRECTION.DOWN) return DIRECTION.UP;
+    if (direction === DIRECTION.LEFT) return DIRECTION.RIGHT;
+}
+
+function getNextNodeDirection(node, currentDirection) {
+    if (!node.next) return currentDirection;
+    const { row: currentRow, column: currentColumn } = node.value;
+    const { row: nextRow, column: nextColumn } = node.next.value;
+
+    if (nextRow === currentRow && nextColumn === currentColumn + 1) return DIRECTION.RIGHT;
+    if (nextRow === currentRow && nextColumn === currentColumn - 1) return DIRECTION.LEFT;
+    if (nextRow === currentRow + 1 && nextColumn === currentColumn) return DIRECTION.DOWN;
+    if (nextRow === currentRow - 1 && nextColumn === currentColumn) return DIRECTION.UP;
+
+    return '';
 }
 
 function createBoard(boardSize) {
