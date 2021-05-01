962798d8b2cd927642bb4024994267ac40773f79
vitorrafael <vrsilveira07@gmail.com>
2021-05-01 15:46:58 -0300

    Converts LinkedList to Typescript

--- src/components/board/LinkedList.ts
+++ src/components/board/LinkedList.ts
@@ -1,26 +1,32 @@
 
 class LinkedListNode {
-    constructor(value) {
+    public value: number;
+    public next: LinkedListNode | null;
+
+    constructor(value: number) {
         this.value = value;
         this.next = null;
     }
 }
 
 export class LinkedList {
-    constructor(value) {
+    public head: LinkedListNode;
+    public tail: LinkedListNode;
+
+    constructor(value: number) {
         const node = new LinkedListNode(value);
         this.head = node;
         this.tail = node;
     }
 
-    insertAtTail(value) {
+    insertAtTail(value: number) {
         const newTail = new LinkedListNode(value);
         const currentTail = this.tail;
         this.tail = newTail;
         this.tail.next = currentTail;
     }
 
-    insertAtHead(value) {
+    insertAtHead(value: number) {
         const node = new LinkedListNode(value);
         const currentHead = this.head;
         this.head = node;
