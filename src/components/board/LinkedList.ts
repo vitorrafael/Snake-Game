2b553a6c71482da5d6d7b37f66ea59cc02ff7348
vitorrafael <vrsilveira07@gmail.com>
2021-05-01 22:06:03 -0300

    Converts application to Typescript

--- src/components/board/LinkedList.ts
+++ src/components/board/LinkedList.ts
@@ -1,32 +1,31 @@
+export class LinkedListNode<Type> {
+    public value: Type;
+    public next: LinkedListNode<Type> | null;
 
-class LinkedListNode {
-    public value: number;
-    public next: LinkedListNode | null;
-
-    constructor(value: number) {
+    constructor(value: Type) {
         this.value = value;
         this.next = null;
     }
 }
 
-export class LinkedList {
-    public head: LinkedListNode;
-    public tail: LinkedListNode;
+export class LinkedList<Type> {
+    public head: LinkedListNode<Type>;
+    public tail: LinkedListNode<Type>;
 
-    constructor(value: number) {
+    constructor(value: Type) {
         const node = new LinkedListNode(value);
         this.head = node;
         this.tail = node;
     }
 
-    insertAtTail(value: number) {
+    insertAtTail(value: Type) {
         const newTail = new LinkedListNode(value);
         const currentTail = this.tail;
         this.tail = newTail;
         this.tail.next = currentTail;
     }
 
-    insertAtHead(value: number) {
+    insertAtHead(value: Type) {
         const node = new LinkedListNode(value);
         const currentHead = this.head;
         this.head = node;
--- src/utils/Utils.js
+++
@@ -1,3 +0,0 @@
-export function generateRandomNumberInRange(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
